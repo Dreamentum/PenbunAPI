9ee495480c4425f1f0d0d83b1f054923a9c90226
cookievirus <cookievirus@gmail.com>
2025-05-22 15:21:12 +0700

    use pointer string -> *string at  model of publisherType

--- models/publisherType.go
+++ models/publisherType.go
@@ -4,8 +4,8 @@ type PublisherType struct {
 	PublisherTypeID string  `json:"publisher_type_id"`
 	TypeName        string  `json:"type_name"`
 	Description     *string `json:"description"`
-	UpdateBy        string  `json:"update_by"`
-	UpdateDate      string  `json:"update_date"`
+	UpdateBy        *string `json:"update_by"`
+	UpdateDate      *string `json:"update_date"`
 	IDStatus        bool    `json:"id_status"`
 	IsDelete        bool    `json:"is_delete"`
 }
