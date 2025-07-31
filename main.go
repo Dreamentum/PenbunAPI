3d3285cc2558070554f8e249fa2b3a49a3cc3e4e
cookievirus <cookievirus@gmail.com>
2025-07-31 12:06:07 +0700

    update PenbunAPI to v.1.8.0

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func main() {
 		StrictRouting:     true,
 		EnablePrintRoutes: true,
 		ServerHeader:      "Fiber",
-		AppName:           "PENBUN API v1.7.9",
+		AppName:           "PENBUN API v1.8.0",
 	})
 
 	// ✅ Serve favicon.ico
