5f287e3294c18f3ff8eb6e622b989f2518abce2d
cookievirus <cookievirus>
2025-08-25 19:44:34 +0700

    just merge

--- controllers/productTypeGroup.go
+++ controllers/productTypeGroup.go
@@ -197,11 +197,11 @@ func DeleteProductTypeGroupByID(c *fiber.Ctx) error {
 	}
 
 	query := `
-		UPDATE tb_product_type
+		UPDATE tb_product_type_group
 		SET is_delete = 1,
 		    update_by = @UpdateBy,
 		    update_date = CAST(SYSDATETIMEOFFSET() AT TIME ZONE 'SE Asia Standard Time' AS DATETIME)
-		WHERE product_type_id = @ID
+		WHERE group_code = @ID
 	`
 	err := utils.ExecuteTransaction(config.DB, []func(tx *sql.Tx) error{
 		func(tx *sql.Tx) error {
