7c5180b7bd391f6d9f6a1f0461ebb4d1c9821afc
cookievirus <cookievirus@gmail.com>
2025-08-29 16:25:54 +0700

    fix ProductType API following DB Structure #03

--- models/productType.go
+++ models/productType.go
@@ -3,12 +3,13 @@ package models
 import "time"
 
 type ProductType struct {
-	ProductTypeID string     `json:"product_type_id"`
-	TypeName      string     `json:"type_name"`
-	TypeGroupName string     `json:"type_group_name"`
-	Description   string     `json:"description"`
-	UpdateBy      string     `json:"update_by"`
-	UpdateDate    *time.Time `json:"update_date,omitempty"`
-	IDStatus      string     `json:"id_status"`
-	IsDelete      bool       `json:"is_delete"`
+	ProductTypeID     string     `json:"product_type_id"`
+	TypeName          string     `json:"type_name"`            // เก็บรหัสกลุ่ม (อ้างอิง tb_product_type_group.group_code) จากคอลัมน์ p.type_group_name
+	TypeNameGroupCode string     `json:"type_name_group_code"` // ชื่อกลุ่มจากการ JOIN กับ tb_product_type_group.group_name
+	GroupName         string     `json:"group_name"`
+	Description       string     `json:"description"`
+	UpdateBy          string     `json:"update_by"`
+	UpdateDate        *time.Time `json:"update_date,omitempty"`
+	IDStatus          bool       `json:"id_status"` // BIT
+	IsDelete          bool       `json:"is_delete"` // BIT
 }
